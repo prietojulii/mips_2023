80171c9124d602f9779bc0ca7c0ec4f86b4bc70f
Mateo Merino <66422394+teomerino1@users.noreply.github.com>
2023-02-22 16:16:06 -0300

    Update IDEX.v

--- mips_2023.srcs/sources_1/new/IDEX.v
+++ mips_2023.srcs/sources_1/new/IDEX.v
@@ -98,75 +98,75 @@ begin
         rs <= 0;
         rt <= 0;
         rd <= 0;
-        shamt_exte <= 0;
+        shamt_extend <= 0;
         imm_extend <= 0;
         data_A <= 0;
         data_B <= 0;
-        pc <= 0;
-        pc_nex <= 0;
+        pc4 <= 0;
+        pc_next <= 0;
         funct  <= 0;
         op <= 0;
-        ctrl_EX_regDEST_fl <= 0;
+        ctrl_EX_regDEST_flag <= 0;
         ctrl_EX_ALU_source_B_flag  <= 0;
         ctrl_EX_ALU_source_A_flag  <= 0;
         ctrl_EX_ALUop_flag <= 0;
-        ctrl_EX_Branch_fla <= 0;
+        ctrl_EX_Branch_flag <= 0;
         ctrl_MEM_memWrite_flag <= 0;
         ctrl_MEM_memRead_flag  <= 0;
-        ctrl_MEM_byte_half_or_word_fla <= 0;
-        ctrl_MEM_wr_reg_fl <= 0;
+        ctrl_MEM_byte_half_or_word_flag <= 0;
+        ctrl_MEM_wr_reg_flag <= 0;
         ctrl_WB_memToReg_flag  <= 0;
-        ctrl_WB_wr_fla <= 0;
+        ctrl_WB_wr_flag <= 0;
     end
     else
     begin
         rs <= i_rs;
         rt <= i_rt;
         rd <= i_rd;
-        shamt_exte <= i_shamt_exte;
+        shamt_extend <= i_shamt_extend;
         imm_extend <= i_imm_extend;
         data_A <= i_data_A;
         data_B <= i_data_B;
-        pc <= i_pc;
-        pc_nex <= i_pc_nex;
+        pc4 <= i_pc4;
+        pc_next <= i_pc_next;
         funct  <= i_funct ;
         op <= i_op;
-        ctrl_EX_regDEST_fl <= i_ctrl_EX_regDEST_fl;
+        ctrl_EX_regDEST_flag <= i_ctrl_EX_regDEST_flag;
         ctrl_EX_ALU_source_B_flag  <= i_ctrl_EX_ALU_source_B_flag ;
         ctrl_EX_ALU_source_A_flag  <= i_ctrl_EX_ALU_source_A_flag ;
         ctrl_EX_ALUop_flag <= i_ctrl_EX_ALUop_flag;
-        ctrl_EX_Branch_fla <= i_ctrl_EX_Branch_fla;
+        ctrl_EX_Branch_flag <= i_ctrl_EX_Branch_flag;
         ctrl_MEM_memWrite_flag <= i_ctrl_MEM_memWrite_flag;
         ctrl_MEM_memRead_flag  <= i_ctrl_MEM_memRead_flag ;
-        ctrl_MEM_byte_half_or_word_fla <= i_ctrl_MEM_byte_half_or_word_fla;
-        ctrl_MEM_wr_reg_fl <= i_ctrl_MEM_wr_reg_fl;
+        ctrl_MEM_byte_half_or_word_flag <= i_ctrl_MEM_byte_half_or_word_flag;
+        ctrl_MEM_wr_reg_flag <= i_ctrl_MEM_wr_reg_flag;
         ctrl_WB_memToReg_flag  <= i_ctrl_WB_memToReg_flag ;
-        ctrl_WB_wr_fla <= i_ctrl_WB_wr_fla;
+        ctrl_WB_wr_flag <= i_ctrl_WB_wr_flag;
     end
 end
 //OUTPUTS
 assign o_rs = rs;
 assign o_rt = rt;
 assign o_rd = rd;
-assign o_shamt_exte = shamt_exte;
+assign o_shamt_extend = shamt_extend;
 assign o_imm_extend = imm_extend;
 assign o_data_A = data_A;
 assign o_data_B = data_B;
-assign o_pc = pc;
-assign o_pc_nex = pc_nex;
+assign o_pc4 = pc4;
+assign o_pc_next = pc_next;
 assign o_funct  = funct ;
 assign o_op = op;
-assign o_ctrl_EX_regDEST_fl = ctrl_EX_regDEST_fl;
+assign o_ctrl_EX_regDEST_flag = ctrl_EX_regDEST_flag;
 assign o_ctrl_EX_ALU_source_B_flag  = ctrl_EX_ALU_source_B_flag ;
 assign o_ctrl_EX_ALU_source_A_flag  = ctrl_EX_ALU_source_A_flag ;
 assign o_ctrl_EX_ALUop_flag = ctrl_EX_ALUop_flag;
-assign o_ctrl_EX_Branch_fla = ctrl_EX_Branch_fla;
+assign o_ctrl_EX_Branch_flag = ctrl_EX_Branch_flag;
 assign o_ctrl_MEM_memWrite_flag = ctrl_MEM_memWrite_flag;
 assign o_ctrl_MEM_memRead_flag  = ctrl_MEM_memRead_flag ;
-assign o_ctrl_MEM_byte_half_or_word_fla = ctrl_MEM_byte_half_or_word_fla;
-assign o_ctrl_MEM_wr_reg_fl = ctrl_MEM_wr_reg_fl;
+assign o_ctrl_MEM_byte_half_or_word_flag = ctrl_MEM_byte_half_or_word_flag;
+assign o_ctrl_MEM_wr_reg_flag = ctrl_MEM_wr_reg_flag;
 assign o_ctrl_WB_memToReg_flag  = ctrl_WB_memToReg_flag ;
-assign o_ctrl_WB_wr_fla = ctrl_WB_wr_fla;
+assign o_ctrl_WB_wr_flag = ctrl_WB_wr_flag;
 
 
 
