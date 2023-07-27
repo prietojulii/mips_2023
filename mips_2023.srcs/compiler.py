5ad5a27df3d525d16e9409ecb1ff73d6a7ea07a3
jprieto <jprieto@ascentio.com.ar>
2023-07-27 20:34:59 -0300

    docs: se agrega programa ejemplo.

--- mips_2023.srcs/compiler.py
+++ mips_2023.srcs/compiler.py
@@ -9,14 +9,6 @@ Inst_J = { "j": 2, "jal": 3}
 Inst_J_R = {"jr": 8, "jalr": 9}
 Inst_HALT = {"halt": 63}
 
-"""
-registros = {"$zero": 0, "$at": 1, "v0": 2, "v1": 3, "a0": 4, "a1": 5, "a2": 6, "a3": 7,
-			"$t0": 8, "$t1": 9, "$t2": 10, "$t3": 11, "$t4": 12, "$t5": 13, "$t6": 14, "$t7": 15, "$t10": 24, "$t9": 25,
- 			"$s0": 16,"$s1": 17, "$s2": 18,"$s3": 19,"$s4": 20, "$s5": 21, "$s6": 22, "$s7": 23,
- 			"$k0": 26,"$k7": 27,
- 			"$gp": 28,"$sp": 29, "$fp": 30, "$ra": 31}
-"""
-
 registros = {"R0": 0, "R1": 1, "R2": 2, "R3": 3, "R4": 4, "R5": 5, "R6": 6, "R7": 7, "R8": 8, "R9": 9, "R10": 10, "R11": 11, "R12": 12,
 			"R13": 13, "R14": 14, "R15": 15, "R16": 16, "R17": 17, "R18": 18, "R19": 19, "R20": 20, "R21": 21, "R22": 22, "R23": 23, "R24": 24,
 			"R25": 25, "R26": 26, "R27": 27, "R28": 28, "R29": 29, "R30": 30, "R31": 31}
@@ -130,13 +122,8 @@ for item in programa:
 			initial.write_file(initial.bin_to_hex(inst_ass), file)
 
 	if (inst[0] in Inst_HALT):
-		# op = initial.dec_to_bin(Inst_HALT[inst[0]], 6)
-		
-		# inm = "0000000000000000"
-		# inst_ass = initial.assemble_instructionI(op, "00000", "00000", inm)
 		inst_ass="00000000000000000000000000000000"
 		initial.write_file(initial.bin_to_hex(inst_ass), file)
-
 
 	if (inst[0] in Inst_L_S):		
 		op = initial.dec_to_bin(Inst_L_S[inst[0]],6)
@@ -148,7 +135,6 @@ for item in programa:
 
 		inst_ass = initial.assemble_instructionI(op, rs, rt, inm)
 		initial.write_file(initial.bin_to_hex(inst_ass), file)
-		
 
 	if (inst[0] in Inst_J):
 		op  = initial.dec_to_bin(Inst_J[inst[0]],6)
